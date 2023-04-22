98abc25d6fd805cfdd7f88b419a2722e76bdfbc9
DURGESH KUMAR <68071906+aditya4-4@users.noreply.github.com>
2023-04-22 10:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for(let i=1;i<=20;i++){
 	else if( i % 5 == 0 && i % 3 != 0){
 		alert("Buzz");
 	}
-	else if( i % 3 == 0 && i % 35!= 0){
+	else if( i % 3 == 0 && i % 5!= 0){
 		alert("Fizz");
 	}
 	else{
